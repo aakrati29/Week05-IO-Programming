30b31d33896d096c069b5dca98742ded260f791b
Aakrati Barsaiyan <aakratibarsaiyan@gmail.com>
2025-02-19 12:07:20 +0530

    [Aakrati] add. Merge two JSON objects into one

---
+++ src/main/java/com/ioprogramming/day02jasondatahandling/practiceproblems/mergetwojsonobjectsintoone/MergeFiles.java
@@ -0,0 +1,27 @@
+package com.ioprogramming.day02jasondatahandling.practiceproblems.mergetwojsonobjectsintoone;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import java.io.File;
+import java.io.IOException;
+
+public class MergeFiles {
+    public static void main(String[] args) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+
+            JsonNode json1 = objectMapper.readTree(new File("D:\\Capgemini\\Week05-IO-Programming\\src\\main\\java\\com\\ioprogramming\\day02jasondatahandling\\practiceproblems\\mergetwojsonobjectsintoone\\Data1.json"));
+            JsonNode json2 = objectMapper.readTree(new File("D:\\Capgemini\\Week05-IO-Programming\\src\\main\\java\\com\\ioprogramming\\day02jasondatahandling\\practiceproblems\\mergetwojsonobjectsintoone\\Data2.json"));
+
+            ObjectNode mergedJson = objectMapper.createObjectNode();
+            mergedJson.setAll((ObjectNode) json1);
+            mergedJson.setAll((ObjectNode) json2);
+
+            System.out.println(objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(mergedJson));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
